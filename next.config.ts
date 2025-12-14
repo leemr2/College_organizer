8fa17443c26d3b341e9503280215aba969302267
leemr2 <markrlee74@gmail.com>
2025-12-14 08:02:42 -0800

    fix: update Next.js to 16.0.10 to fix CVE-2025-66478

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    // Temporarily ignore ESLint errors during builds
-    // ESLint is configured and can be run separately with `npm run lint`
-    // This allows the build to succeed while ESLint issues are being addressed
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
